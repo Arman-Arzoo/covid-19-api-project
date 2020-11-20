1fef3dcb15cfe7f1c0fc7e0445996d1b6285ea99
Arman-Arzoo <51259303+Arman-Arzoo@users.noreply.github.com>
2020-11-20 13:57:39 +0500

    fetch data is showed in frontend

--- covid-19-frontend/src/component/Info.js
+++ covid-19-frontend/src/component/Info.js
@@ -3,20 +3,27 @@ import { useEffect, useState } from "react";
 
 export const Info = () => {
   const [globalData, setGlobaldata] = useState({});
+  const [update , setupdate] = useState({});
 
   useEffect(() => {
     async function getCovidData() {
       try {
         const response = await fetch("https://covid19.mathdro.id/api");
         const data = await response.json();
+       
         const covidData = {
           confirmed: data.confirmed,
           recovered: data.recovered,
           deaths: data.deaths,
-          lastUpdate: data.lastUpdate,
         };
-        console.log(covidData);
+        const updateDate ={
+          lastUpdate:data.lastUpdate,
+          
+        }
+       
         setGlobaldata(covidData);
+        setupdate(updateDate);
+        
       } catch (err) {
         console.log("error fetching data");
       }
@@ -24,29 +31,26 @@ export const Info = () => {
     getCovidData();
   }, []);
 
+ 
   return (
     <div className="info-container">
-      {/* {Object.keys(globalData).map((val,ind)=>{
+      {Object.keys(globalData).map((val,ind)=>{
           return(
           <div key={ind}>
             <h2> {val}</h2>
             <h3>{globalData[val].value}</h3>
+            
           </div>
           )
-      })} */}
+      })}
+      
+      <div>
+        <h2>Last update</h2>
+    <h3>{update.lastUpdate}</h3>
+      </div>
 
-      <div>
-        <h2>Confirmed Case</h2>
-      </div>
-      <div>
-      <h2>Recoverd Case</h2>
-      </div>
-      <div>
-      <h2>Deaths Case</h2>
-      </div>
-      <div>
-      <h2>last update</h2>
-      </div>
+      
+
     </div>
   );
 };
